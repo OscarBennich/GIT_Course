502b39e8f4e6abae968bb267707d60376063eb0a
David Ahlin Abarca <david.ahlin.abarca@santanderconsumer.se>
2018-09-27 13:47:33 +0200

    okidoki

--- GitTestApp/Program.cs
+++ GitTestApp/Program.cs
@@ -4,11 +4,19 @@ namespace GitTestApp
 {
     class Program
     {
+<<<<<<< HEAD
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             Console.WriteLine("Hello GIT!");
             Console.WriteLine("Byebye baby!");
+=======
+            Console.WriteLine("Godbye GIT!");
+>>>>>>> e4d035f4a4a8151c5b9fda092219adc1e33f43ae
             Console.ReadLine();
         }
+=======
+        //Xianjie here!
+>>>>>>> 706feec84b58723ca69b393e4fa28819813ce12a
     }
 }
