e49aeeaed9e3c63784bf83e636eac638f8322e10
erikisaksson <erik.isaksson@santanderconsumer.se>
2018-09-27 13:40:31 +0200

    nyteknik

--- GitTestApp/Program.cs
+++ GitTestApp/Program.cs
@@ -9,7 +9,12 @@ namespace GitTestApp
         {
             Console.WriteLine("Godbye GIT!");
             Console.ReadLine();
+			
+			
         }
+		public static void DeleteWin32Locally(){
+			//do the things
+		}
 =======
         //Xianjie here!
 >>>>>>> 706feec84b58723ca69b393e4fa28819813ce12a
