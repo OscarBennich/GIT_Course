0b9bd39fa74021a42fd1a59f2bf195c1de13ec22
Niklas Fridell <W100568@internal.financecorp.biz>
2018-09-27 13:41:16 +0200

    niklas

--- GitTestApp/Program.cs
+++ GitTestApp/Program.cs
@@ -4,14 +4,16 @@ namespace GitTestApp
 {
     class Program
     {
-<<<<<<< HEAD
+
         static void Main(string[] args)
         {
             Console.WriteLine("Godbye GIT!");
             Console.ReadLine();
         }
-=======
+
         //Xianjie here!
->>>>>>> 706feec84b58723ca69b393e4fa28819813ce12a
+        706feec84b58723ca69b393e4fa28819813ce12a
     }
 }
+
+//Niklas kommenterar
