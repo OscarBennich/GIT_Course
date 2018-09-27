a48ad8ec0d8135db850696c85368cd1bd4d54dc5
Xianjie Zhang <W100557@internal.financecorp.biz>
2018-09-27 13:42:28 +0200

    Change

--- GitTestApp/Program.cs
+++ GitTestApp/Program.cs
@@ -4,14 +4,11 @@ namespace GitTestApp
 {
     class Program
     {
-<<<<<<< HEAD
         static void Main(string[] args)
         {
             Console.WriteLine("Godbye GIT!");
             Console.ReadLine();
+			//Change
         }
-=======
-        //Xianjie here!
->>>>>>> 706feec84b58723ca69b393e4fa28819813ce12a
     }
 }
